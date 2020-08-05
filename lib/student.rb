05d16ce1c422660794bb10b5bda0b6bd02062760
Catherine Price <catprice444@gmail.com>
2020-08-05 10:22:18 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -39,7 +39,7 @@ class Student
       
     DB[:conn].execute(SQL, self.name, self.grade)
     
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students") 
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
 
   def self.create
