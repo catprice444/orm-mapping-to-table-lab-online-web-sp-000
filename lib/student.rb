45775adbf0ac89645d255cf8f04396cb96e34267
Catherine Price <catprice444@gmail.com>
2020-08-05 10:09:39 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,7 +12,7 @@ class Student
   end 
   
   def self.create_table
-    sql = <<_SQL 
+    sql = <<-SQL 
       CREATE TABLE IF NOT EXISTS students (
         id INTEGER PRIMARY KEY
         name TEXT
