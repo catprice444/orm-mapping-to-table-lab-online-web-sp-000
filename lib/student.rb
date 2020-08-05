cb1a74be515173fdce4d0a59a2d15f54815bfbc8
Catherine Price <catprice444@gmail.com>
2020-08-05 10:03:30 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,13 @@
 class Student
+  attr_accessor: name, album
+  attr_reader: id
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
-  def initialize (@)
+  def initialize (name, album, id=nil)
+    @name = name
+    @album = album
+    @id = id
+  end 
   
 end
